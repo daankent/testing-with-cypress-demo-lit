1d78e3b3f23293be5afe12d466019a18ab6a0541
daankent <daankentrop@gmail.com>
2025-12-24 14:55:15 +0100

    Add first e2e test for a11y

---
+++ cypress/e2e/a11y.cy.js
@@ -0,0 +1,20 @@
+const pages = ["/"];
+const sizes = ["iphone-6", "ipad-2", [1024, 768], "macbook-13", "samsung-s10"];
+
+describe("Axe Test all pages", () => {
+  pages.forEach((page) => {
+    sizes.forEach((size) => {
+      it(`Should be accesible on ${size} screen`, () => {
+        if (Cypress._.isArray(size)) {
+          cy.viewport(size[0], size[1]);
+        } else {
+          cy.viewport(size);
+        }
+
+        cy.visit("http://localhost:5173" + page);
+        cy.injectAxe();
+        cy.checkA11y();
+      });
+    });
+  });
+});
