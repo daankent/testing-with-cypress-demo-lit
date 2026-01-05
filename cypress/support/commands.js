ecf05f5693732b4dc895beb5ba0e4675c0386eb8
daankent <daankentrop@gmail.com>
2026-01-05 21:04:56 +0100

    Remove example text

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,12 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
 import { printAccessibilityViolations } from "./axe";
 
 Cypress.Commands.add("checkA11yWithUi", (context, options) => {
